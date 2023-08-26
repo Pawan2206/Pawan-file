62d4d99b71cae30a13c296d286fa5eceea2abb50
Pawan2206 <143156442+Pawan2206@users.noreply.github.com>
2023-08-26 09:20:57 +0530

    Check Prime Number

---
+++ Assignment.js
@@ -0,0 +1,17 @@
+let num = 5;
+let fact = 0;
+for (let i=0;i<=num;i++)
+{
+if (num % i == 0)
+{
+fact++;
+}
+}
+if (fact == 2)
+{
+console.log("Prime Number");
+}
+else
+{
+console.log("Not A Prime Number");
+}
